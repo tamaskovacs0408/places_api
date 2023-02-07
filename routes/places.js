8be6780b66beb07ab9fa1d9c1ac5e70c1a860273
Tamás Kovács <kovacs.tamas0408@gmail.com>
2023-02-07 20:44:17 +0100

    Create routes

--- routes/places.js
+++ routes/places.js
@@ -1,5 +1,17 @@
 const express = require("express");
 const path = require("path");
-const mongooseMethods = require("./mongoose");
+const placesController = require("./mongoose");
 
 const router = express.Router();
+
+router.get('/places', placesController.getPlaces);
+
+router.get('/places/:pId', placesController.getOnePlace);
+
+router.post('/places/create', placesController.createPlace);
+
+router.put('/places/:pId', placesController.updatePlace);
+
+router.delete('/places/:pId', placesController.deletePlace);
+
+module.exports = router;

0a133be0fbb297e4461e12a48017a538ca3d7bab
Tamás Kovács <kovacs.tamas0408@gmail.com>
2023-02-07 20:47:08 +0100

    Update import path

--- routes/places.js
+++ routes/places.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const path = require("path");
-const placesController = require("./mongoose");
+const placesController = require('../controllers/mongooseController');
 
 const router = express.Router();
 

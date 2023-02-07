6e17304869b4163ce202c695550f225ca59755d1
Tamás Kovács <kovacs.tamas0408@gmail.com>
2023-02-07 20:44:28 +0100

    Add router to app

--- app.js
+++ app.js
@@ -2,44 +2,21 @@ const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
 const xss = require('xss-clean');
-// const fs = require('fs');
-//const mongoMethods = require('./mongo');
-const mongooseMethods = require('./mongoose');
 
-// const api = require('./datas.json');
+const placeRoutes = require('./routes/places');
+
 
 const app = express()
 
 const PORT = 8080;
+
+app.use(placeRoutes);
 
 app.use(helmet());
 app.use(cors());
 app.use(xss());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-
-// app.get('/', (req, res) => {
-//   res.json(api);
-// });
-
-// With MongoDB code:
-
-// app.post('/places', mongoMethods.createPlaces);
-
-// app.get('/places', mongoMethods.getPlaces)
-
-// With Mongoose Schema/models:
-
-app.post('/places/create', mongooseMethods.createPlace);
-
-app.get("/places", mongooseMethods.getPlaces);
-
-app.get("/places/:pId", mongooseMethods.getOnePlace);
-
-app.put("/places/:pId", mongooseMethods.updatePlace);
-//app.patch("/places/:pId", mongooseMethods.updatePlace);
-
-app.delete("/places/:pId", mongooseMethods.deletePlace);
 
 app.listen(PORT, () => {
   `Server runs at port ${PORT}.`

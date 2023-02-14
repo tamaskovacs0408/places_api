e885c8ad840ce8178df313b8ff927d9d5a6871c4
Tamás Kovács <kovacs.tamas0408@gmail.com>
2023-02-14 11:59:15 +0100

    Change routes

--- routes/places.js
+++ routes/places.js
@@ -5,14 +5,14 @@ const router = express.Router();
 
 router.get('/', placesController.getWelcome);
 
-router.get('/places', placesController.getPlaces);
+router.get('/api/places', placesController.getPlaces);
 
-router.get('/places/:pId', placesController.getOnePlace);
+router.get('/api/places/:pId', placesController.getOnePlace);
 
-router.post('/create', placesController.createPlace);
+router.post('/api/places/create', placesController.createPlace);
 
-router.put('/places/:pId', placesController.updatePlace);
+router.put('/api/places/:pId', placesController.updatePlace);
 
-router.delete('/places/:pId', placesController.deletePlace);
+router.delete('/api/places/:pId', placesController.deletePlace);
 
 module.exports = router;
